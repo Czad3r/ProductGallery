a68aebb50828f93f080bd9d302958dcdbb614ffd
czader98 <czader98@poczta.fm>
2018-09-15 20:18:40 +0200

    First commit, product class is ready, I've started creating a window

---
+++ src/productGallery/Product.java
@@ -0,0 +1,40 @@
+package productGallery;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+public class Product {
+    private int id;
+    private String name;
+    private float price;
+    private Date addDate;
+    private byte[] picture;
+
+    public Product(int id,String name,float price,Date addDate,byte[] picture){
+        this.id=id;
+        this.name=name;
+        this.price=price;
+        this.addDate=addDate;
+        this.picture=picture;
+    }
+
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public float getPrice(){
+        return price;
+    }
+
+    public Date getAddDate() {
+        return addDate;
+    }
+
+    public byte[] getPicture() {
+        return picture;
+    }
+
+
+}
