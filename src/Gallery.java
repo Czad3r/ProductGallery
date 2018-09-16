647569dff2f81d75575285b6b320a3b1c23dc480
czader98 <czader98@poczta.fm>
2018-09-16 15:12:25 +0200

    Added JDatePicker library
    https://www.codejava.net/java-se/swing/how-to-use-jdatepicker-to-display-calendar-component

--- src/Gallery.java
+++ src/Gallery.java
@@ -1,8 +1,22 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class Gallery {
+public class Gallery extends JFrame {
     private JPanel panel1;
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
+
+    public Gallery() {
+        setContentPane(panel1);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(1000,500);
+    }
+
+    public static void main(String[] args) {
+        new Gallery().setVisible(true);
+    }
 }
+
+
